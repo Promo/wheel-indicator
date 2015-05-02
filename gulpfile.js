19ab13301e7578663d9d17eb8587caf661a1c017
Anton Grischenko <f0rmat1k@yandex-team.ru>
2015-05-03 02:29:17 +0500

    Сборка jquery версии

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var gfi = require("gulp-file-insert");
 
 gulp.task('compress', function() {
     gulp.src('lib/*.js')
@@ -9,4 +10,22 @@ gulp.task('compress', function() {
             extname: '.min.js'
         }))
         .pipe(gulp.dest('dist'))
+});
+
+gulp.task('jquery', function () {
+    return gulp.src('jquery/jquery.tmp.js')
+        .pipe(gfi({
+            "/* Wheel-indicator */": "lib/wheel-indicator.js"
+        }))
+        .pipe(rename({
+            basename: 'wheel-indicator',
+            prefix: 'jquery.',
+            extname: '.js'
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(rename({
+            extname: '.min.js'
+        }))
+        .pipe(gulp.dest('dist'));
 });
